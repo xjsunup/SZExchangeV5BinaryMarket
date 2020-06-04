b785640c63848f348a8eea60a65f053f653b5f77
xjsun <sxjjio@163.com>
2020-06-04 11:00:06 +0800

    删除idea

--- src/main/java/exchange/sz/v5/binary/gateway/SZV5MessageFacade.java
+++ src/main/java/exchange/sz/v5/binary/gateway/SZV5MessageFacade.java
@@ -74,7 +74,10 @@ public class SZV5MessageFacade implements SZV5Gateway {
     @SZV5Message(msgType = SZV5MsgType.LEVEL1)
     @Override
     public void onLevel1Market(SZV5Level1MarketSnapshot level1MarketSnapshot) {
-        log.info("level1 -> {}", level1MarketSnapshot);
+        String securityId = level1MarketSnapshot.getSecurityId().getValue();
+        if(securityId.equals("100303") || securityId.equals("100504")){
+            log.info("level1 -> {}", level1MarketSnapshot);
+        }
     }
 
     @Slf4j
